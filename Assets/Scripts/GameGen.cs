cb3a17d5bf45873d2052a357a11d9502e50f1f11
Danboe2009 <Danboe2009@gmail.com>
2017-01-26 13:21:30 -0500

    Added Everything.

---
+++ Assets/Scripts/GameGen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGen : MonoBehaviour {
+
+
+	public Square[,] Board;
+	public Square square;
+	// Use this for initialization
+	void Start () {
+		Board = new Square[10,10];
+		SetUpBoard ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void SetUpBoard(){
+		for (int x = 0; x < 10; x++) {
+			for (int y = 0; y < 10; y++) {
+				Square tempSquare = Instantiate<Square> (square);
+				tempSquare.transform.SetParent (this.transform);
+				tempSquare.transform.position = new Vector3 (x, y, -0.1f);
+				tempSquare.Type = 0;
+				//Debug.Log ("Board X Is " + x);
+				//Debug.Log ("Board Y Is " + y);
+				Board [x,y] = tempSquare;
+			}
+		}
+		Board [0, 9].Type = 1;
+		Board [9, 0].Type = 2;
+		Board [5, 5].Type = 3;
+	}
+}
---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour {
+
+	// Use this for initialization
+	public int Type;
+
+	public Sprite Red;
+	public Sprite Blue;
+	public Sprite Dust;
+
+	void Start () {
+		if (Type == 1) {
+			GetComponent<SpriteRenderer> ().sprite = Red;
+		} else if (Type == 2) {
+			GetComponent<SpriteRenderer> ().sprite = Blue;
+		} else if (Type == 3) {
+			GetComponent<SpriteRenderer> ().sprite = Dust;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
