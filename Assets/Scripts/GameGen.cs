47f966825612164728164d8bae489def6e3ddfe8
Danboe2009 <Danboe2009@gmail.com>
2017-01-27 12:28:53 -0500

    Started working on player movement.

--- Assets/Scripts/GameGen.cs
+++ Assets/Scripts/GameGen.cs
@@ -6,14 +6,22 @@ public class GameGen : MonoBehaviour {
 
 
 	public Square[,] Board;
+	public int[,] BoardPos;
 	public Square square;
+	public bool debug;
 
-	private int playerPosX;
-	private int playerPosY;
+	private static int playerPosX;
+	private static int playerPosY;
+	private static int enemyPosX;
+	private static int enemyPosY;
 	// Use this for initialization
 	void Start () {
 		Board = new Square[10,10];
-		SetUpBoard ();
+		if (!debug) {
+			playerPosX = 9;
+			playerPosY = 0;
+			SetUpBoard ();
+		}
 	}
 	
 	// Update is called once per frame
@@ -25,39 +33,56 @@ public class GameGen : MonoBehaviour {
 		for (int x = 0; x < 10; x++) {
 			for (int y = 0; y < 10; y++) {
 				Square tempSquare = Instantiate<Square> (square);
-				tempSquare.transform.SetParent (this.transform);
+				tempSquare.transform.SetParent (gameObject.transform);
 				tempSquare.transform.position = new Vector3 (x, y, -0.1f);
-				tempSquare.Type = 0;
+				tempSquare.Type = "0";
 				//Debug.Log ("Board X Is " + x);
 				//Debug.Log ("Board Y Is " + y);
 				Board [x,y] = tempSquare;
 			}
 		}
-		Board [0, 9].Type = 1;
-		Board [9, 0].Type = 2;
-		//Board [5, 5].Type = 3;
-
-		playerPosX = 9;
-		playerPosY = 0;
+		Board [enemyPosX, enemyPosY].Type = "1";
+		Board [playerPosX, playerPosY].Type = "2";
+		//Board [5, 5].Type = "3";
 	}
 
 	private void AvaliableMoves(){
-		Debug.Log ("playerPosX " + playerPosX);
-		Debug.Log ("playerPosY " + playerPosY);
+		//Debug.Log ("playerPosX " + playerPosX);
+		//Debug.Log ("playerPosY " + playerPosY);
 		//Debug.Log("playerPosX + 1 " + (playerPosX + 1));
 		//Debug.Log("playerPosX - 1 " + (playerPosX - 1));
 		if ((playerPosX + 1) < 10) {
-			Board [playerPosX + 1, playerPosY].Type = 4;
+			Board [playerPosX + 1, playerPosY].Type = "4R";
 			//Debug.Log ("Type" + Board [playerPosX + 1, playerPosY].Type);
 		} 
 		if ((playerPosX - 1) > 0) {
-			Board [playerPosX - 1, playerPosY].Type = 4;
+			Board [playerPosX - 1, playerPosY].Type = "4L";
 		}
 		if ((playerPosY + 1) < 10) {
-			Board [playerPosX, playerPosY + 1].Type = 4;
+			Board [playerPosX, playerPosY + 1].Type = "4U";
 		} 
 		if ((playerPosY - 1) > 0) {
-			Board [playerPosX, playerPosY - 1].Type = 4;
+			Board [playerPosX, playerPosY - 1].Type = "4D";
 		}
 	}
+
+	public static void MovePlayer(string dir)
+	{
+		switch (dir) {
+		case "U":
+			playerPosY++;
+			break;
+		case "D":
+			playerPosY--;
+			break;
+		case "L":
+			playerPosX--;
+			break;
+		case "R":
+			playerPosX++;
+			break;
+		default:
+			break;
+		}
+	} 
 }
--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Square : MonoBehaviour {
 
 	// Use this for initialization
-	public int Type;
+	public string Type;
 
 	public Sprite Red;
 	public Sprite Blue;
@@ -18,17 +18,47 @@ public class Square : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		UpdateSquare ();
+	}
+		
+	void OnMouseDown()
+	{
+		// this object was clicked - do something
+		Debug.Log ("Hit " + Type);
+		//Destroy (this.gameObject);
+		switch (Type) {
+		case "4U":
+			GameGen.MovePlayer ("U");
+			break;
+		case "4L":
+			GameGen.MovePlayer ("L");
+			break;
+		case "4R":
+			GameGen.MovePlayer ("R");
+			break;
+		case "4D":
+			GameGen.MovePlayer ("D");
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void UpdateSquare(){
 		switch(Type) {
-		case 1:
+		case "1":
 			GetComponent<SpriteRenderer> ().sprite = Red;
 			break;
-		case 2:
+		case "2":
 			GetComponent<SpriteRenderer> ().sprite = Blue;
 			break;
-		case 3:
+		case "3":
 			GetComponent<SpriteRenderer> ().sprite = Dust;
 			break;
-		case 4:
+		case "4U":
+		case "4L":
+		case "4R":
+		case "4D":
 			GetComponent<SpriteRenderer> ().sprite = Green;
 			break;
 		default:
